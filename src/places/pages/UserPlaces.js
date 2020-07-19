dc7dc3d6a2beec5b0df60e48231f86a0a2f43913
Dordb <43724442+Dordb@users.noreply.github.com+5915265+dorx48@gmail.com>
2020-07-19 18:06:22 +0200

    Map Ok

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,37 +1,40 @@
 import React from "react";
 import PlaceList from "../components/PlaceList";
+import { useParams } from "react-router-dom";
+
+const DUMMY_PLACES = [
+  {
+    id: "place1",
+    title: "Tour 5",
+    description: "Une Tour 5",
+    imageUrl:
+      "https://www.sortiraparis.com/images/80/83517/438334-visuel-paris-tour-eiffel-19.jpg",
+    address: "5st rue de la pls",
+    location: {
+      lat: 40.7808,
+      lng: -73.9772,
+    },
+    creator: "5",
+  },
+  {
+    id: "place30",
+    title: "Basment 0",
+    description: "Un Basment",
+    imageUrl:
+      "https://www.basementwaterproofingetc.com/images/contemporary-basement.jpg",
+    address: "Piiiilolloolo",
+    location: {
+      lat: 40,
+      lng: 0,
+    },
+    creator: "5",
+  },
+];
 
 function UserPlaces(props) {
-  const DUMMY_PLACES = [
-    {
-      id: "place1",
-      title: "Tour 5",
-      description: "Une Tour 5",
-      imageUrl:
-        "https://www.sortiraparis.com/images/80/83517/438334-visuel-paris-tour-eiffel-19.jpg",
-      address: "5st rue de la pls",
-      location: {
-        lat: 40,
-        lng: -50,
-      },
-      creator: "5",
-    },
-    {
-      id: "place30",
-      title: "Basment 0",
-      description: "Un Basment",
-      imageUrl:
-        "https://www.basementwaterproofingetc.com/images/contemporary-basement.jpg",
-      address: "Piiiilolloolo",
-      location: {
-        lat: 10,
-        lng: -80,
-      },
-      creator: "5",
-    },
-  ];
-
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 }
 
 export default UserPlaces;
